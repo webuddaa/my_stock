93fd03fd1fa75847c30e83508aa5a346b296460a
xiangfeng <xuxf5@yonghui.cn>
2022-03-13 11:34:51 +0800

    添加发送邮件的代码

--- bin/run_select_stock.sh
+++ bin/run_select_stock.sh
@@ -3,12 +3,9 @@ path="/xiangfeng/my_stock"
 rm ${path}/temp/*
 
 cd ${path}
-echo 'starting'
+
 venv/bin/python -m src.run_files.run_select_stock
-echo 'starting two'
+
 zip -r ${path}/temp.zip ${path}/temp
 
-echo 'starting end'
 venv/bin/python -m src.run_files.run_send_mail_attachment
-
-echo 'success'
