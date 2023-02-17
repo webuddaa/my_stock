23ea348dbef3326aebaedae9e26fe436d47bd032
xiangfeng <xuxf5@yonghui.cn>
2023-02-17 19:06:30 +0800

    新增期货背驰筛选

--- src/futures/run_select_futures.py
+++ src/futures/run_select_futures.py
@@ -4,20 +4,17 @@
 @file_name: run_select_futures.py
 """
 import time
+import re
 import pandas as pd
-from datetime import datetime
 from loguru import logger
 import requests
 import json
-from retrying import retry
 
-from src.config.common_config import FUTURE_SYMBOLS
 from src.stock.divergence import Divergence
 from src.stock.indicator import cal_macd
 from src.utils.message_utils import send_wechat_msg
 
 
-@retry(stop_max_attempt_number=20)
 def futures_zh_minute_sina(symbol: str, period: str) -> pd.DataFrame:
     """
     :param symbol: AP2305
@@ -25,7 +22,7 @@ def futures_zh_minute_sina(symbol: str, period: str) -> pd.DataFrame:
     """
     url = "https://stock2.finance.sina.com.cn/futures/api/jsonp.php/=/InnerFuturesNewService.getFewMinLine"
     params = {"symbol": symbol, "type": period}
-    time.sleep(0.5)
+    time.sleep(1.5)
     r = requests.get(url, params=params)
     temp_df = pd.DataFrame(json.loads(r.text.split("=(")[1].split(");")[0]))
     temp_df.columns = ["Date", "Open", "High", "Low", "Close", "Volume", "Hold"]
@@ -40,14 +37,13 @@ def futures_zh_minute_sina(symbol: str, period: str) -> pd.DataFrame:
     return temp_df2
 
 
-@retry(stop_max_attempt_number=20)
 def futures_zh_daily_sina(symbol: str) -> pd.DataFrame:
     """
     :param symbol: AP2305
     """
     url = "https://stock2.finance.sina.com.cn/futures/api/jsonp.php/=/InnerFuturesNewService.getDailyKLine"
     params = {"symbol": symbol}
-    time.sleep(0.5)
+    time.sleep(1.5)
     r = requests.get(url, params=params)
     temp_df = pd.DataFrame(json.loads(r.text.split("=(")[1].split(");")[0]))
     temp_df.columns = ["Date", "Open", "High", "Low", "Close", "Volume", "Hold", "Settle"]
@@ -90,49 +86,55 @@ def get_k_lines(symbol: str, period: str):
     return pd.concat([temp_df2, temp_df])
 
 
-def get_symbol_list(target: str) -> list:
-    """获取某个品种当前所有的可能合约"""
-    dt = datetime.now()
-
-    this_year = dt.year % 100
-    this_month = dt.month
-
-    result = []
-    for m in [f"{i:02d}" for i in range(1, 13)]:
-        if int(m) == this_month:
-            continue
-        if int(m) < this_month:
-            result.append(f"{target}{this_year + 1}{m}")
-        else:
-            result.append(f"{target}{this_year}{m}")
-
-    return result
+def convert_symbol(temp_symbol: str):
+    temp_symbol2 = temp_symbol.upper()
+    a1 = ''.join(re.findall(r'[A-Za-z]', temp_symbol2))
+    a2 = temp_symbol2.split(a1)[1]
+    return f"{a1}2{a2}" if len(a2) == 3 else f"{a1}{a2}"
 
 
-def fun(period):
+def extract_symbol(x):
+    if "(" in x:
+        return x.split("(")[1].split(")")[0]
+    return
+
+
+def get_symbol_list() -> list:
+    """获取当前可以交易的所有合约代码"""
+    url = "https://www.9qihuo.com/qihuoshouxufei"
+    r = requests.get(url)
+    temp_df = pd.read_html(r.text)[0]
+    temp_df.columns = ["合约品种", "现价", "涨/跌停板", "保证金-买开", "保证金-卖开", "保证金-保证金/每手",
+                       "手续费标准-开仓", "手续费标准-平昨", "手续费标准-平今", "每跳毛利", "手续费(开+平)",
+                       "每跳净利", "备注", "-", "-"]
+    temp_df["temp_symbol"] = temp_df["合约品种"].apply(extract_symbol)
+    temp_df2 = temp_df[["temp_symbol"]].dropna()
+    temp_df2["new_symbol"] = temp_df2["temp_symbol"].apply(convert_symbol)
+    return list(temp_df2["new_symbol"].unique())
+
+
+def fun(period: str, all_symbols: list):
     logger.info(f"开始查询{period}级别背驰的合约")
     result = []
-    for target in FUTURE_SYMBOLS:
-        symbol_list = get_symbol_list(target)
 
-        for symbol in symbol_list:
-            temp_df = get_k_lines(symbol, period)
-            if temp_df.shape[0] < 150:
-                continue
-            logger.info(f"symbol={symbol}, 数据集大小: {temp_df.shape[0]}")
-            temp_df2 = cal_macd(temp_df)
-            divergence = Divergence(temp_df2)
-            divergence.merge_macd()
-            if divergence.bottom_divergence():
-                result.append(symbol)
+    for symbol in all_symbols:
+        temp_df = get_k_lines(symbol, period)
+        if temp_df.shape[0] < 150:
+            continue
+        logger.info(f"symbol={symbol}, 数据集大小: {temp_df.shape[0]}")
+        temp_df2 = cal_macd(temp_df)
+        divergence = Divergence(temp_df2)
+        divergence.merge_macd()
+        if divergence.bottom_divergence():
+            result.append(symbol)
     return result
 
 
 if __name__ == '__main__':
     logger.add(f"./log_files/run_select_futures.log", retention='10 days')
 
+    all_symbol_list = get_symbol_list()
     for p in ["day", "60", "30", "15", "5", "1"]:
-        res_list = fun(p)
+        res_list = fun(p, all_symbol_list)
         content = f"级别: {p} | 底背驰的期货合约: {res_list}"
         send_wechat_msg(content, job_num_list=["81145511"])
-
