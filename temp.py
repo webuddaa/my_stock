73359976b31d0cdc3bcd224b8f1ca40cfa80e96d
xiangfeng <xuxf5@yonghui.cn>
2023-02-21 09:09:16 +0800

    新增期货背驰筛选

--- temp.py
+++ temp.py
@@ -7,3 +7,18 @@ import akshare as ak
 import re
 
 
+def get_futures_current_pirce(symbol: str = "AP2305") -> float:
+    """
+    获取该合约当前的价格
+    symbol: AP2305
+    """
+    target = ''.join(re.findall(r'[A-Za-z]', symbol))
+    market = "FF" if target in ("IF", "IH", "IC", "IM", "TS", "TF", "T") else "CF"
+
+    temp_df = ak.futures_zh_spot(symbol=symbol, market=market, adjust='0')
+    current_price = float(temp_df.loc[0, "current_price"])
+    return current_price
+
+
+if __name__ == '__main__':
+    print(get_futures_current_pirce())
