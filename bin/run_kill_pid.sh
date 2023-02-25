3488706a6f4999f290307cf7fb65fef42cb0cb24
xiangfeng <xuxf5@yonghui.cn>
2023-02-25 21:16:41 +0800

    新增期货背驰筛选

--- bin/run_kill_pid.sh
+++ bin/run_kill_pid.sh
@@ -1,2 +1,2 @@
 
-ps x | grep '/data/Projects/or-eta-test/target/app_buddaa.jar' | grep -v grep | awk '{print $1}' | xargs kill -9
+ps x | grep 'run_select_futures_15m_5m' | grep -v grep | awk '{print $1}' | xargs kill -9
