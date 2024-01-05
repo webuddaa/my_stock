8f084ed0c81473ce742dca9df493537f28a0b235
xxf_windows <buddaa@163.com>
2024-01-05 20:17:53 +0800

    添加发送邮件

--- src/run_files/run_future_final_info.py
+++ src/run_files/run_future_final_info.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from src.config.common_config import PATH, DAY_ONLY_SYMBOL_LIST
 from src.futures.futures_basis_func import get_futures_recent_price, percent_fun, cal_fea
-from src.utils.message_utils import send_wechat_msg, send_wechat_file
+from src.utils.message_utils import send_wechat_msg, send_wechat_file, my_send_email
 
 if __name__ == '__main__':
     try:
@@ -33,14 +33,25 @@ if __name__ == '__main__':
         df4["手续费/保证金"] = df4["交易所手续费"] / df4["每手保证金"]
         df4["手续费/保证金"] = df4["手续费/保证金"].apply(percent_fun)
 
-        df5 = df4[["品种中文", "合约代码", "收盘价", "成交额(21-23)(亿元)", "成交额(09-15)(亿元)",
+        df5 = df4[["品种中文", "合约代码", "成交额(21-23)(亿元)", "成交额(09-15)(亿元)",
                    "每手保证金", "交易所手续费", "最小跳动的浮亏比例", "手续费/保证金",
                    "最小变动价位", "合约乘数", "交易所保证金", "手续费-开仓", "手续费-平今",
-                   "是否主力合约", "交易所", "成交量(21-23)", "成交量(09-15)"]]
+                   "是否主力合约", "交易所", "收盘价", "成交量(21-23)", "成交量(09-15)"]]
 
         temp_path = f"{PATH}/log_files/期货合约日维度数据汇总_{datetime.now().strftime('%Y%m%d')}.xlsx"
         df5.to_excel(temp_path, header=True, index=False, encoding='utf-8-sig')
         send_wechat_file(temp_path)
+
+        msg = """
+        临江仙·缠中说禅
+        浊水倾波三万里，愀然独坐孤峰。龙潜狮睡候飙风。无情皆竖子，有泪亦英雄。 
+        长剑倚天星斗烂，古今过眼成空。乾坤俯仰任穷通。半轮沧海上，一苇大江东。
+        """
+        recipients = ["buddaa@foxmail.com", "263146874@qq.com"]
+        if datetime.now().isoweekday() == 5:
+            my_send_email("期货合约基本信息整理", msg, recipients, attachments_path=temp_path)
+        else:
+            my_send_email("期货合约基本信息整理", msg, "buddaa@foxmail.com", attachments_path=temp_path)
 
     except Exception as e:
         logger.exception(e)
